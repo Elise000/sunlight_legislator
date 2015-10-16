5b11cc5a64644a68a38d5eb7114fa7cf4c957618
wengkhing <kheryee@live.com>
2015-10-16 18:23:29 +0800

    writen active record queries

--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -4,16 +4,34 @@ require 'byebug'
 
 class Legislator < ActiveRecord::Base
 
+
+  validates :phone, :format => { :with => /\A\d{3}\D\d{3}\D\d{4}\z/, :message => "error phone"}
+
   def self.check(term, arg)
-
     self.where("#{term} = ?", arg)
-
   end
 
-  def self.bystate(state, title)
+  # def self.bystate(state, title)
+  #   self.where(state: state, title: title).limit(2)
+  # end
 
-    self.where(state: state, title: title).limit(2)
+  def self.bystate(state, arg)
+    a = self.where("#{state} = ?", arg)
 
+    senators = a.where(title: "Sen").order(:lastname)
+
+    puts "Senators:"
+    senators.all.each do |x|
+      puts x.firstname + " " + x.lastname + " " + "(#{x.party})"
+    end
+
+  puts ".................................."
+    rep = a.where(title: "Rep").order(:lastname)
+
+    puts "Representatives:"
+    rep.all.each do |x|
+      puts x.firstname + " " + x.lastname + " " + "(#{x.party})"
+    end
   end
 
   def self.list(term1, term2, arg)
@@ -22,13 +40,32 @@ class Legislator < ActiveRecord::Base
 
   end
 
-  def self.list(term1, term2, term3, arg)
+  def self.list3(term1, term2, term3, arg)
 
       a = self.where(["#{term1} = ? and #{term2} = ? and #{term3}=?", arg[0], arg[1],arg[2]])
 
       a.all.each do |x|
        puts x.party + " " + x.title + " " + x.in_office
        end
+  end
+
+  def self.bygender?(arg)
+      a = self.where(in_office: "1")
+      sen_count = a.where(title: "Sen").all.count
+      rep_count = a.count - sen_count
+      gender_sen_cnt = a.where("gender = ? and title = ?", arg, "Sen").count
+      percent = (gender_sen_cnt/sen_count.to_f) * 100
+
+      gender_rep_cnt = a.where("gender = ? and title = ?", arg, "Rep").count
+      percentage = (gender_rep_cnt/rep_count.to_f) * 100
+
+      if arg == "M"
+        puts "Male Senators: " + gender_sen_cnt.to_s + "(#{percent}%)"
+        puts "Male Representatives: " + gender_rep_cnt.to_s + "(#{percentage}%)"
+      else
+        puts "Female Senators: " + gender_sen_cnt.to_s + "(#{percent}%)"
+        puts "Female Representatives: " + gender_rep_cnt.to_s + "(#{percentage}%)"
+      end
   end
 
   def self.at_office?(term, arg)
@@ -42,8 +79,54 @@ class Legislator < ActiveRecord::Base
         puts "#{a.firstname} is out"
         return false
       end
+  end
+
+  def self.list_all
+    # num = 0
+    a = self.where(in_office: "1")
+    # puts a.count
+    arr = a.uniq.pluck(:state)
+
+    new_arr = []
+
+    arr.each do |x|
+      sen_count = a.where(title: "Sen", state: x).count
+      rep_count = a.where(title: "Rep", state: x).count
+      # num += (sen_count + rep_count)
+      if sen_count > 0 && rep_count > 0
+        new_arr << [x , sen_count, rep_count]
+      end
+        # puts "#{x}:  " + sen_count.to_s + " Senators, " + rep_count.to_s + " " + "Representatives(s)"
+    end
+
+    sorted_arr = new_arr.sort{|a, b| b[2]<=> a[2]}
+
+    sorted_arr.each do |element|
+      puts "#{element[0]}:  " + element[1].to_s + " Senators, " + element[2].to_s + " " + "Representatives(s)"
+    end
+  end
+
+  def self.count
+    sen_count = self.where(title:"Sen").all.count
+    rep_count = self.where(title:"Rep").all.count
+
+    puts "Senators:   " + sen_count.to_s
+    puts "Representatives:   " + rep_count.to_s
 
   end
+
+  def self.delete
+    puts "All records: " + self.all.count.to_s
+    puts self.where(in_office: "0").count.to_s
+    self.destroy_all(in_office: "0")
+
+    puts
+    puts "All records after delete: " + self.all.count.to_s
+  end
+end
+
+
+
 # attr_accessor :first_name, :last_name, :birthday
 
   # validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :message => "error"}
@@ -68,4 +151,3 @@ class Legislator < ActiveRecord::Base
   # end
 # implement your Student model here
 
-end
--- main.rb
+++ main.rb
@@ -1,20 +1,26 @@
 require_relative 'app/models/legislator'
 require_relative 'app'
 
-puts "Putting results"
-p Legislator.where(state: "NY")
-puts "...................................."
+# puts "Putting results"
+# p Legislator.where(state: "NY")
+# puts "...................................."
 
-p Legislator.where(state: "NY", title: "Sen").limit(2)
+# p Legislator.where(state: "NY", title: "Sen").limit(2)
 
-p Legislator.check("state", "NY")
-p Legislator.bystate("NY", "Sen")
+# p Legislator.check("state", "NY")
+# p Legislator.bystate("NY", "Sen")
 
-p Legislator.list("party", "title",["R", "Sen"])
-Legislator.list("party", "title", "in_office" ,["R", "Sen","1"])
+# p Legislator.list("party", "title",["R", "Sen"])
+# Legislator.list3("party", "title", "in_office" ,["R", "Sen","1"])
 
-a = Legislator.check("title","Rep").first
-puts a.congresspedia_url + " , " + a.firstname
+# a = Legislator.check("title","Rep").first
+# puts a.congresspedia_url + " , " + a.firstname
 
-Legislator.at_office?("title","Rep")
+# Legislator.at_office?("title","Rep")
+
+#Legislator.bystate("state","NY")
+ # Legislator.bygender?("M")
+ # Legislator.list_all
+ # Legislator.count
+  Legislator.delete
 
